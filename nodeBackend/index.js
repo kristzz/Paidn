b748e778958d6570b02f8d983991ae8d1ed78c81
kristzz <emils.krists@gmail.com>
2024-02-14 00:53:50 +0200

    project setup

---
+++ nodeBackend/index.js
@@ -0,0 +1,22 @@
+require('dotenv').config();
+
+const { Pool } = require('pg');
+const pool = new Pool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
+});
+
+const express = require('express');
+const app = express();
+const port = process.env.PORT ||  3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(port, () => {
+  console.log(`App listening at http://localhost:${port}`);
+});
