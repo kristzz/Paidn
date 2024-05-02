932c9800829c2cd9abfa277935f1a2ef8093e2b4
kristzz <emils.krists@gmail.com>
2024-05-03 01:22:26 +0300

    register axios done

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -3,6 +3,9 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import axios from 'axios';
+
+axios.defaults.baseURL = 'http://127.0.0.1:8000/api';
 
 const app = createApp(App)
 
--- frontend/src/views/Register.vue
+++ frontend/src/views/Register.vue
@@ -3,50 +3,67 @@
 
 <template>
     <main>
-        <container>
-            <form class="input-container">
-                <input class="small-input" type="email" placeholder="email@example.com" v-model="email">
-                <input class="small-input" type="password" placeholder="password" v-model="password">
-                <input class="small-input" type="password" placeholder="confirm password" v-model="password_confirmation">
-                <div class="row">
-                  <div class="stateButton weight-regular height-text" :class="buttonClass" @click="changeState">{{ buttonText }}</div>
-                  <button id="authButton" class="authButton weight-regular height-text">Sign Up</button>
-                </div>
-                <div class="left">
-                  <router-link to="/login" id="left" class="authButton" style="color: inherit;">Login instead</router-link>
-                </div>
-            </form>
-        </container>
+      <div>
+        <form class="input-container">
+            <input class="small-input" type="email" placeholder="email@example.com" v-model="email">
+            <input class="small-input" type="password" placeholder="password" v-model="password">
+            <input class="small-input" type="password" placeholder="confirm password" v-model="password_confirmation">
+            <div class="row">
+              <div class="stateButton weight-regular height-text" :class="buttonClass" @click="changeType">{{ buttonText }}</div>
+              <button type="button" @click="register" id="authButton" class="authButton weight-regular height-text">Sign Up</button>
+            </div>
+            <div class="left">
+              <router-link to="/login" id="left" style="color: inherit;">Login instead</router-link>
+            </div>
+        </form>
+      </div>
     </main>
 </template>
 
 
 <script>
+import axios from 'axios';
+
 export default {
  data() {
     return {
         email: '',
         password: '',
         password_confirmation: '',
-        state: 'unselected', 
+        type: 'unselected', 
     };
  },
- computed: {
-    buttonClass() {
-      return `state-${this.state.toLowerCase()}`;
-    },
-    buttonText() {
-      return this.state.charAt(0).toUpperCase() + this.state.slice(1);
-    },
-  },
+
  methods: {
-    changeState() {
-      const states = ['user', 'business'];
-      const currentIndex = states.indexOf(this.state);
-      const nextIndex = (currentIndex + 1) % states.length;
-      this.state = states[nextIndex];
+  register() {
+    axios.post('/register', {
+      email: this.email,
+      password: this.password,
+      password_confirmation: this.password_confirmation,
+      type: this.type
+    }).then((response) => {
+      console.log(response.data);
+      this.$router.push('/login');
+    }).catch((error) => {
+      console.log(error.message);
+    });
+  },
+  changeType() { 
+      const types = ['user', 'business'];
+      const currentIndex = types.indexOf(this.type);
+      const nextIndex = (currentIndex + 1) % types.length;
+      this.type = types[nextIndex];
     },
  },
+
+ computed: {
+    buttonClass() {
+      return `state-${this.type.toLowerCase()}`;
+    },
+    buttonText() {
+      return this.type.charAt(0).toUpperCase() + this.type.slice(1);
+    },
+  },
 };
 </script>
 
@@ -100,5 +117,6 @@ main{
 
 #left{
   margin: 0.5rem 2rem 0.75rem 0rem;
+  user-select: none;
 }
 </style>
