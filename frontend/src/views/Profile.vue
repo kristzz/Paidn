563b2289934499cb014bbf43a0246344c6dad214
kristzz <emils.krists@gmail.com>
2024-05-04 01:40:55 +0300

    added fetch methods for user info and education info

--- frontend/src/views/Profile.vue
+++ frontend/src/views/Profile.vue
@@ -8,6 +8,13 @@
     <div v-else>
         <p>Loading...</p>
     </div>
+    <h1>Education</h1>
+    <div v-if="educationData && educationData.length > 0">
+        <div v-for="educationEntry in educationData" :key="educationEntry.id">
+            <p>Provider: {{ educationEntry.educationProvider }}</p>
+            <p>Level: {{ educationEntry.levelOfEducation }}</p>
+        </div>
+    </div>
 </template>
 
 <script>
@@ -16,13 +23,15 @@ import axios from 'axios';
 export default {
   data(){
     return{
-        profileData: []
+        profileData: {},
+        educationData: []
     };
   },
   mounted(){
     const authToken = localStorage.getItem('authToken');
     if(authToken) {
         this.fetchProfile(authToken);
+        this.fetchEducation(authToken);
     }else {
         alert('You are not logged in');
         this.$router.push('/login');
@@ -35,13 +44,23 @@ export default {
                     Authorization: `Bearer ${authToken}`
                 }
             }).then((response) => {
-                console.log('Response Data:', response.data);
                 this.profileData = response.data.userData;
-                console.log('Profile Data:', this.profileData);
             }).catch((error) => {
                 console.log(error.message);
             });
-        }
+        },
+        fetchEducation(authToken) {
+            axios.get('/getEducation', {
+                headers: {
+                    Authorization: `Bearer ${authToken}`
+                }
+            }).then((response) => {
+                this.educationData = response.data.education;
+                console.log('Education Data Fetched:', this.educationData);
+            }).catch((error) => {
+                console.log(error.message);
+            });
+        },
     }
 }
 </script>
