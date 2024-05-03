9e714104787bd15a94d92b8f6ec0d1f728cb1c3e
kristzz <emils.krists@gmail.com>
2024-05-04 01:07:34 +0300

    made login functionality

--- frontend/src/views/Login.vue
+++ frontend/src/views/Login.vue
@@ -1,8 +1,29 @@
 
 <script>
-export default {
+import axios from 'axios';
 
-}
+export default {
+    data() {
+        return {
+            email: '',
+            password: '',
+        };
+    },
+    methods: {
+        login() {
+            axios.post('/login', {
+                email: this.email,
+                password: this.password,
+            }).then((response) => {
+                localStorage.setItem('authToken', response.data.token);
+                axios.defaults.headers.common['Authorization'] = `Bearer ${response.data.token}`;
+                this.$router.push('/home');
+            }).catch((error) => {
+                console.log(error.message);
+            });
+        },
+    },
+};
 </script>
 
 
@@ -10,8 +31,9 @@ export default {
     <main>
         <container>
             <form>
-                <input type="email" placeholder="email@example.com">
-                <input type="password" placeholder="password">
+                <input type="email" placeholder="email@example.com" v-model="email">
+                <input type="password" placeholder="password" v-model="password">
+                <button type="button" @click="login" >Login</button>
             </form>
         </container>
     </main>
