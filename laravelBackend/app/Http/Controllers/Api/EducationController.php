924e0171c38b9bc5e06d25f2fb579f8fc4622fca
kristzz <emils.krists@gmail.com>
2024-05-06 01:45:19 +0300

    added a way to edit education experience

--- laravelBackend/app/Http/Controllers/Api/EducationController.php
+++ laravelBackend/app/Http/Controllers/Api/EducationController.php
@@ -47,6 +47,38 @@ class EducationController extends Controller
         ]);
     }
 
+    public function editEducationExperience(Request $request){
+        $education = Education::find($request->id);
+
+        $request->validate([
+            'educationProvider' => 'required',
+            'levelOfEducation' => 'required',
+            'fieldOfStudy' => 'required',
+            'description' => 'nullable',
+            'startDate' => 'required',
+            'endDate' => 'required',
+        ]);
+        if (Auth::user()->type === 'user') {
+            $education->educationProvider = $request->educationProvider;
+            $education->levelOfEducation = $request->levelOfEducation;
+            $education->fieldOfStudy = $request->fieldOfStudy;
+            $education->description = $request->description;
+            $education->startDate = $request->startDate;
+            $education->endDate = $request->endDate;
+            $education->save();
+
+            return response()->json([
+                'status' => true,
+                'message' => 'Education updated successfully'
+            ]);
+        } else {
+            return response()->json([
+                'status' => false,
+                'message' => 'Only user users can edit education experience'
+            ]);
+        }
+    }
+
     public function deleteEducation(Request $request){
         $education = Education::find($request->id);
         $education->delete();
