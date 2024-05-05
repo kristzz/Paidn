a9cb40ea47d8407dfe8d22552e8bb9d3c992035f
kristzz <emils.krists@gmail.com>
2024-05-06 01:19:23 +0300

    added a way for regularUser to see job postings

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,10 +1,46 @@
 <template>
-  <h1>Majas</h1>
+   <div v-if="allPosts && allPosts.length > 0">
+    <div v-for="post in allPosts" :key="post.id">
+      <p>Title: {{ post.title }}</p>
+      <p>Profession: {{ post.profession }}</p>
+      <p>Description: {{ post.jobDesc }}</p>
+      <p>Salary range : {{ post.salaryRangeLowest }}$ - {{ post.salaryRangeHighest }}$</p>
+      <br>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {
+import axios from 'axios';
 
+export default {
+  data(){
+    return{
+      allPosts : []
+    };
+  },
+  mounted(){
+    const authToken = localStorage.getItem('authToken');
+    if(authToken) {
+      this.fetchPosts(authToken);
+    }else {
+      alert('You are not logged in');
+      this.$router.push('/login');
+    }
+  },
+  methods: {
+    fetchPosts(authToken){
+      axios.get('/getPosts', {
+        headers: {
+          Authorization: `Bearer ${authToken}`
+        }
+      }).then((response) => {
+        this.allPosts = response.data.posts;
+      }).catch((error) => {
+        console.log(error.message);
+      });
+    }
+  }
 }
 </script>
 
