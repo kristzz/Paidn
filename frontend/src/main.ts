ee2118b3a108690ab9f53f850b14228938639f66
kristzz <emils.krists@gmail.com>
2024-05-21 22:55:24 +0300

    added a func for error displaying globally

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -9,6 +9,20 @@ axios.defaults.baseURL = 'http://127.0.0.1:8000/api';
 
 const app = createApp(App)
 
+app.config.globalProperties.$handleError = function (error: any) {
+    let errorMessage = 'An error occurred';
+    if (error.response && error.response.data.errors) {
+        const errors = error.response.data.errors;
+        const firstErrorKey = Object.keys(errors)[0];
+        errorMessage = errors[firstErrorKey][0];
+    } else if (error.request) {
+        errorMessage = 'No response from server';
+    } else {
+        errorMessage = error.message;
+    }
+    return errorMessage;
+  };
+
 app.use(router)
 
 app.mount('#app')
