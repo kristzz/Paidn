1c1b4ed64c1048b7beb51759af2637123bb2cad1
kristzz <emils.krists@gmail.com>
2024-05-30 00:58:28 +0300

    added nav bar

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,3 +1,15 @@
 <template>
+  <navbar />
   <router-view />
 </template>
+
+
+<script>
+  import Navbar from './components/navbar.vue';
+
+  export default {
+    components: {
+      Navbar
+    }
+  };
+</script>
