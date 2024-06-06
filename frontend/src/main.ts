f3363e1828f58a2d819aa3d7adb04f559ea56eab
kristzz <emils.krists@gmail.com>
2024-06-06 12:05:44 +0300

    kkas

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -5,7 +5,7 @@ import router from './router';
 import axios from 'axios';
 
 // Set the base URL for Axios
-axios.defaults.baseURL = 'http://127.0.0.1:8000/api';
+axios.defaults.baseURL = 'api-12dkgabrielis@kvalifikacija.rvt.lv/api';
 
 // Define a global error handler for Axios
 axios.interceptors.response.use(
